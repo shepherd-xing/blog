fe87944f67805996474c1851de02235ac714dbf1
xing <1741807421@qq.com>
2018-07-10 10:27:31 +0800

    第二章 标签分类

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Comment
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25, label='名字')
+    email = forms.EmailField(label='邮箱')
+    to = forms.EmailField(label='发送到')
+    comments = forms.CharField(required=False, widget=forms.Textarea, label='评论')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
---
+++ blog/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
+# Create your views here.
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(1)
+    return render(request, 'blog/post/list.html', {'page':page, 'posts':posts, 'tag':tag})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    context = {'post':post, 'comments':comments, 'new_comment':new_comment, 'comment_form':comment_form,
+               'similar_posts':similar_posts}
+    return render(request, 'blog/post/detail.html', context)
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            # Read "get_absolute_url()" at http://127.0.0.1:8000/blog/2018/07/07/get_absolute_url/    可点击
+            #post_url = post.get_absolute_url()   #Read "get_absolute_url()" at /blog/2018/07/07/get_absolute_url/ 无法点击
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'shepherd-xing@outlook.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+        cd = None
+    return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent, 'cd':cd})
