a7c0b706c0780e32cad0867ff7494809b1497651
xing <1741807421@qq.com>
2018-07-29 12:10:04 +0800

    API权限

--- api/views.py
+++ api/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from api.serializers import PostSerializer
 from blog.models import Post
 
 # Create your views here.
-class PostListView(ListAPIView):
+class PostListView(ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
-class PostDetailView(RetrieveAPIView):
+class PostDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
